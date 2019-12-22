1594f0f0b2b0d7ec90db6fac748c443c2fe3bdc3
gphirke <50782307+gphirke@users.noreply.github.com>
2019-12-22 14:57:41 +0530

    Update subArraySum.js

--- subArraySum.js
+++ subArraySum.js
@@ -24,4 +24,21 @@ var subarraySum = function(nums, k) {
 };
 
 
+//with some extra space 
 
+var subarraySum = function(nums, k) {
+    var count = 0;
+    var sum = {};
+    sum[0] = 0;
+     for (let i = 1; i <= nums.length; i++){
+         sum[i] = sum[i - 1] + nums[i - 1];
+     }
+    for(let i=0; i<nums.length; i++){
+      for(let j=i+1; j<=nums.length; j++){
+        if(sum[j] - sum[i] == k){
+           ++count;
+        }
+      }  
+    }
+    return count;
+};
