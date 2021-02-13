5eceb9c15d1145a1f602b936f1534fb01666f2bd
gphirke <50782307+gphirke@users.noreply.github.com>
2021-02-13 16:51:03 +0530

    Update sumWithoutPlusOperator.js

--- sumWithoutPlusOperator.js
+++ sumWithoutPlusOperator.js
@@ -6,7 +6,7 @@ function getSum(a, b){
   while(b!=0){
     var carry = a&b;  //calculate if is there any carry we need to add
     a = a^b;   // a is used to hold the sum
-    b = carry<<1;  //b is used to hold the sum 
+    b = carry<<1;  //b is used to hold the carry 
   }
   return a;
 }
