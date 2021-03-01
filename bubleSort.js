2a3d218f78bdb338d3ad8dedc4c093d65e7478ae
gphirke <50782307+gphirke@users.noreply.github.com>
2021-03-01 20:31:38 +0530

    Update bubleSort.js

--- bubleSort.js
+++ bubleSort.js
@@ -1,4 +1,4 @@
-function bubleSort(a){
+function bubleSort1(a){
     let numberOfSwaps = 0;
     for (let i = 0; i < a.length; i++) {
     // Track number of elements swapped during a single array traversal
@@ -24,4 +24,35 @@ function bubleSort(a){
 
 //bubleSort([3,2,1]);
 
-bubleSort([3, 4, 1, 2]);
+bubleSort1([3, 4, 1, 2]);
+
+
+// URL https://www.youtube.com/watch?v=6Gv8vg0kcHc
+
+
+function swap(i, j, arr){
+	var temp =  arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+function bubleSort(arr){
+	var isSorted=false;
+	var lastIndx = arr.length - 1;
+	while(!isSorted){
+		isSorted = true;
+		for(let i=0; i<lastIndx; i++){
+			if(arr[i]<arr[i+1]){
+				swap(i, i+1, arr);
+				isSorted = false;
+			}
+		}
+		--lastIndx;
+	}
+}
+
+var arr = [3, 2, 9, 6, 5, 4];
+
+bubleSort(arr);
+
+console.log(arr);
