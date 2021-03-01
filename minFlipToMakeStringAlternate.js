2344662a7eb6978ed4517d5dd584d1767b3f792d
gphirke <50782307+gphirke@users.noreply.github.com>
2021-03-01 10:54:35 +0530

    Update minFlipToMakeStringAlternate.js

--- minFlipToMakeStringAlternate.js
+++ minFlipToMakeStringAlternate.js
@@ -14,3 +14,33 @@ function minFlipToMakeStringAlternate(arr){
 }
 
 console.log(minFlipToMakeStringAlternate([1,1,1,1,1, 0]));  //o/p will be 2 to make  [1,0,1,0,1, 0]
+
+
+// 2nd solution
+
+function minFlipToMakeStringAlternate(arr){
+	var flip = arr[0];
+	var count = 0;
+
+	var checkFlip = function(f) {
+		var flip = f;
+		for(let i=1; i<arr.length; i++){
+			if(arr[i] != flip){
+				++count;
+			}
+			flip = !flip;
+	 }
+	 return count;	
+	}	
+
+	if (flip == 1) {
+	 return checkFlip(0);
+	}
+
+  if (flip == 0) {
+	 return	checkFlip(1);
+	}
+}
+
+
+console.log(minFlipToMakeStringAlternate([0,1,1,1,1, 0])); 
